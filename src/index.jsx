ac773fa3877555ffe4e17760936f0e54fc290a05
Valentin Chapel <109554149+ValentinChaps@users.noreply.github.com>
2024-01-04 18:29:37 +0100

    Ajout de la photo de couverture avec le texte et mise en place du footer

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './components/Home'
 import Footer from './components/Footer'
-import About from './pages/About'
+import About from './components/About'
 import Header from './components/Header'
 import Error from './components/Error'
 
--- src/pages/Home/index.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <main>
-      <h1>Contenu</h1>
-    </main>
-  )
-}
-
-export default Home
