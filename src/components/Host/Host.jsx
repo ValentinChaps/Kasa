afcb5dd68bff4e367a39f0096eb0ddf8a1058ee4
Valentin Chapel <109554149+ValentinChaps@users.noreply.github.com>
2024-02-16 13:38:31 +0100

    Mise en forme du composant "Host"

--- src/components/Host/Host.jsx
+++ src/components/Host/Host.jsx
@@ -3,7 +3,7 @@ import css from './Host.module.scss'
 function Host({hostName, hostPicture, id}) {
     return (
             <div className={css.container} key={id}>
-                <div><h2>{hostName}</h2></div>
+                <div className={css.hostName}><h2>{hostName}</h2></div>
                 <div> <img src={hostPicture} alt=''></img></div>
             </div>  
     )
